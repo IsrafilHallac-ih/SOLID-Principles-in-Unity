f6e117a74d19f0080bc5c2005cf17d1f3f355edb
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2026-05-06 18:44:43 +0300

    Dependency İnversion Principle
    SOLID principles in Unity Dependency İnversion Principle

---
+++ SOLID/5_DIP/Script/HeavyShellProjectile.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class HeavyShellProjectile : MonoBehaviour, IProjectile
+{
+    public GameObject shellPrefab;
+    public void Fire(Transform firePoint)
+    {
+        GameObject shell = GameObject.Instantiate(shellPrefab, firePoint.position, firePoint.rotation);
+        shell.GetComponent<Rigidbody2D>().AddForce(firePoint.up * 10f, ForceMode2D.Impulse);
+    }
+}
---
+++ SOLID/5_DIP/Script/LightSheelProjectile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LightSheelProjectile : MonoBehaviour,IProjectile
+{
+  public GameObject shellPrefab;
+    public void Fire(Transform firePoint)
+    {
+        GameObject shell = GameObject.Instantiate(shellPrefab, firePoint.position, firePoint.rotation);
+        shell.GetComponent<Rigidbody2D>().AddForce(firePoint.up * 10f, ForceMode2D.Impulse);
+    }
+
+    
+}
